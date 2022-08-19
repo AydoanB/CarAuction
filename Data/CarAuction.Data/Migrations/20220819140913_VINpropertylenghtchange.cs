d599feee0e0cfee3eee7b40972be6a725263d9ec
AydoanB <balakchiev02@gmail.com>
2022-08-19 17:19:57 +0300

    Vin property lenght change

---
+++ Data/CarAuction.Data/Migrations/20220819140913_VINpropertylenghtchange.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarAuction.Data.Migrations
+{
+    public partial class VINpropertylenghtchange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VIN",
+                table: "Cars",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VIN",
+                table: "Cars",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17);
+        }
+    }
+}
