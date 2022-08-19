a89403070c2ae56bc3fa7c6b3e9a7e839125484d
AydoanB <balakchiev02@gmail.com>
2022-08-19 12:18:15 +0300

    Add about page

---
+++ Web/CarAuction.Web/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarAuction.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
