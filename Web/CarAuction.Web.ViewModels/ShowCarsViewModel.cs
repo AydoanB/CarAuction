824a3524c2d42228397333cb8899958e2651d3fb
AydoanB <balakchiev02@gmail.com>
2022-08-22 15:42:23 +0300

    Add VM to map cars for view

--- Web/CarAuction.Web.ViewModels/ShowCarsViewModel.cs
+++ Web/CarAuction.Web.ViewModels/ShowCarsViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace CarAuction.Web.ViewModels
+﻿using System.Collections;
+
+namespace CarAuction.Web.ViewModels
 {
     public class ShowCarsViewModel
     {
         public string Make { get; set; }
         public string Model { get; set; }
         public decimal? StartPrice { get; set; }
+        public int DoorsCount { get; set; }
+        public string Transmission { get; set; }
     }
 }
