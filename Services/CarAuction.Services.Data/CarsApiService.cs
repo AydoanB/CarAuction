5aa7e4cabb33c254e07c7afe929b0743f51cc5cf
AydoanB <balakchiev02@gmail.com>
2022-08-23 08:12:11 +0300

    Add class to make request for data to API

---
+++ Services/CarAuction.Services.Data/CarsApiService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarAuction.Data.Models.CarModel;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace CarAuction.Services.Data
+{
+    public class CarsApiService
+    {
+        public async Task AddCars(string make, int limit)
+        {
+            var client = new RestClient();
+
+            var url = $"https://api.api-ninjas.com/v1/cars?limit={limit}" + $"&make={make}";
+
+
+            var request = new RestRequest(url);
+            request.AddHeader("x-api-key", "c9f5dleiao8N6d+3LmsrmA==qf17CKr6PhhuleZD");
+
+            var response = client.Get(request);
+            //TODO Response is done, problem with json deserializing
+            
+            var serialized = JsonConvert.DeserializeObject<List<Car>>(response.Content);
+
+            foreach (var carDto in serialized)
+            {
+                var car = new Car();
+            }
+        }
+    }
+}
--- Services/CarAuction.Services/Cars/CarsService.cs
+++ Services/CarAuction.Services/Cars/CarsService.cs
@@ -6,6 +6,7 @@ using CarAuction.Data;
 using CarAuction.Data.Models.AuctionModels;
 using CarAuction.Data.Models.CarModel;
 using CarAuction.Data.Models.CarModels;
+using CarAuction.Services.Data;
 using CarAuction.Web.ViewModels;
 
 namespace CarAuction.Services.Cars
@@ -13,25 +14,18 @@ namespace CarAuction.Services.Cars
     public class CarsService : ICarsService
     {
         private readonly ApplicationDbContext dbContext;
+        private readonly CarsApiService carsApi;
 
-        public CarsService(ApplicationDbContext DbContext)
+        public CarsService(ApplicationDbContext DbContext, CarsApiService CarsApi)
         {
             this.dbContext = DbContext;
+            this.carsApi = CarsApi;
         }
 
         public IEnumerable<ShowCarsViewModel> ShowAll()
         {
-            var data = this.dbContext.Cars.Select(x => new ShowCarsViewModel()
-            {
-                Make = x.Model.Manufacturer.Name,
-                Model = x.Model.Name,
-                StartPrice = x.BuyNowPrice,
-                DoorsCount = int.Parse(x.Model.CountOfDoors.ToString()),
-                Transmission = x.Model.Engine.GearBox.ToString()
-            })
-                .ToList();
-
-            return data;
+            var result = this.carsApi.AddCars("audi", 5);
+            return new List<ShowCarsViewModel>();
         }
     }
 }
