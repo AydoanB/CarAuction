ec404a1c45ebc0c878468ea23293f2b9d6b08a16
Aydoan Balakchiev <balakchiev02@gmail.com>
2022-08-29 09:53:17 +0300

    Add dto class for cars

---
+++ Services/CarAuction.Services.Data/JsonImport/CarsImportDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarAuction.Services.Data.JsonImport
+{
+    public class CarsImportDto
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Fuel_Type { get; set; }
+
+        public string Class { get; set; }
+
+        public string Drive { get; set; }
+
+        public double Displacement { get; set; }
+
+        public double Cylinders { get; set; }
+
+        public string Transmission { get; set; }
+
+        public string Year { get; set; }
+    }
+}
