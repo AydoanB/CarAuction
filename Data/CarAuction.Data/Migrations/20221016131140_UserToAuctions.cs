7d0a3dee8cdcdd8fc3894bdd839c29de64e466a2
Aydoan Balakchiev <balakchiev02@gmail.com>
2022-10-16 19:08:26 +0300

    Add user to auction

---
+++ Data/CarAuction.Data/Migrations/20221016131140_UserToAuctions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarAuction.Data.Migrations
+{
+    public partial class UserToAuctions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Auctions",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Auctions_UserId",
+                table: "Auctions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Auctions_AspNetUsers_UserId",
+                table: "Auctions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Auctions_AspNetUsers_UserId",
+                table: "Auctions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Auctions_UserId",
+                table: "Auctions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Auctions");
+        }
+    }
+}
